650ac3e1a43b3e79b512455733035d736ebc24f7
damankaur09 <damankaur209@gmail.com>
2017-12-27 10:32:55 +0530

    add logout option

--- app/src/main/java/com/android/maplocation/geofence/GetMapLocationActivity.java
+++ app/src/main/java/com/android/maplocation/geofence/GetMapLocationActivity.java
@@ -18,6 +18,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -229,6 +230,18 @@ public class GetMapLocationActivity extends AppCompatActivity
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch ( item.getItemId() ) {
+            case R.id.logout: {
+                SharedPreferencesHandler.clearAll(this);
+                finish();
+                return true;
+            }
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
     private final int REQ_PERMISSION = 999;
 
     // Check for permission to access Location
