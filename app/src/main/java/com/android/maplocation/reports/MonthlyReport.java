1af4ddeb9446f1dcfbaad5aa6bca2ff28c598573
damankaur09 <damankaur209@gmail.com>
2018-01-02 15:29:34 +0530

    setting and getting month and year

--- app/src/main/java/com/android/maplocation/reports/MonthlyReport.java
+++ app/src/main/java/com/android/maplocation/reports/MonthlyReport.java
@@ -15,13 +15,17 @@ import android.widget.TextView;
 
 import com.android.maplocation.R;
 
+import java.util.Calendar;
+
 /**
  * Created by Daman on 12/28/2017.
  */
 
 public class MonthlyReport extends Fragment {
 
-    Button textView;
+    Button button;
+    TextView tfDescription,tfDate;
+    private String month,selectyear;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -35,10 +39,17 @@ public class MonthlyReport extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         super.onViewCreated(view, savedInstanceState);
-        textView=view.findViewById(R.id.hello);
-        textView.setOnClickListener(new View.OnClickListener() {
+        tfDescription=view.findViewById(R.id.tv_date);
+        tfDate=view.findViewById(R.id.tv_date1);
+        button =view.findViewById(R.id.hello);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                final Calendar calendar = Calendar.getInstance();
+                int yy = calendar.get(Calendar.YEAR);
+                int mm = calendar.get(Calendar.MONTH);
+                int dd = calendar.get(Calendar.DAY_OF_MONTH);
                 createDialogWithoutDateField().show();
             }
         });
@@ -46,7 +57,7 @@ public class MonthlyReport extends Fragment {
     }
 
     private DatePickerDialog createDialogWithoutDateField() {
-        DatePickerDialog dpd = new DatePickerDialog(getActivity(), null, 2014, 1, 24);
+        DatePickerDialog dpd = new DatePickerDialog(getActivity(), myDateListener, 2018, 0, 30);
         try {
             java.lang.reflect.Field[] datePickerDialogFields = dpd.getClass().getDeclaredFields();
             for (java.lang.reflect.Field datePickerDialogField : datePickerDialogFields) {
@@ -69,4 +80,16 @@ public class MonthlyReport extends Fragment {
         }
         return dpd;
     }
+
+    private DatePickerDialog.OnDateSetListener myDateListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker arg0, int year, int monthOfYear, int dayOfMonth) {
+            selectyear=String.valueOf(year);
+            month=String.valueOf(monthOfYear+1);
+            tfDescription.setText(selectyear);
+            tfDate.setText(month);
+        }
+    };
+
+
 }
